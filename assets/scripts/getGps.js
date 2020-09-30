432d901a4672f7c974c36c5fc901e62b04430e8b
Eliáš Sidon <sidonismo@gmail.com>
2020-09-30 19:40:24 +0200

    function that get the last gps for the maps of mapy.cz

--- assets/scripts/getGps.js
+++ assets/scripts/getGps.js
@@ -1,43 +1,53 @@
 let x = document.getElementById("demo");
-console.log(x);
 const misto = [];
-let noveMisto = new Object;
+let noveMisto = new Object();
 const lokace = navigator.geolocation;
-var coords = {lat: "", lon: ""};
+var coords = { lat: "", lon: "" };
 var options = {
-  enableHighAccuracy: false,
+  enableHighAccuracy: true,
   timeout: 5000,
-  maximumAge: 0
+  maximumAge: 600000,
 };
 function getLocation() {
   if (lokace) {
     return lokace.getCurrentPosition(showPosition, showError, options);
-  } else { 
-    x.innerHTML = "Váš prohlížeč nepodporuje geolokaci.<br>Geolocation is not supported by this browser.";
+  } else {
+    x.innerHTML =
+      "Váš prohlížeč nepodporuje geolokaci.<br>Geolocation is not supported by this browser.";
   }
 }
 
 function showPosition(position) {
-    noveMisto.gps = { 'latitude': position.coords.latitude, 'longitude': position.coords.longitude, 'accuracy': position.coords.accuracy };
-  x.innerHTML = "Zeměpisná šířka (latitude): " + position.coords.latitude + 
-  "<br>Zeměpisná délka (longitude): " + position.coords.longitude + 
-  `<br>Přesnost na ${position.coords.accuracy} metrů.<br>More or less ${position.coords.accuracy} meters.`;
+  noveMisto.gps = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+    accuracy: position.coords.accuracy,
+  };
+  x.innerHTML =
+    "Zeměpisná šířka (latitude): " +
+    position.coords.latitude +
+    "<br>Zeměpisná délka (longitude): " +
+    position.coords.longitude +
+    `<br>Přesnost na ${position.coords.accuracy} metrů.<br>More or less ${position.coords.accuracy} meters.`;
 }
 
 function showError(error) {
-  switch(error.code) {
+  switch (error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML = "Teprve po povolení GPS lokace, je možné přidat místo.<br>User denied the request for Geolocation."
+      x.innerHTML =
+        "Teprve po povolení GPS lokace, je možné přidat místo.<br>User denied the request for Geolocation.";
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "GPS lokace je nedostupná.<br>Location information is unavailable."
+      x.innerHTML =
+        "GPS lokace je nedostupná.<br>Location information is unavailable.";
       break;
     case error.TIMEOUT:
-      x.innerHTML = "Vypršel časový limit pro získání GPS.<br>The request to get user location timed out."
+      x.innerHTML =
+        "Vypršel časový limit pro získání GPS.<br>The request to get user location timed out.";
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML = "Nastala neznámá chyba, zkuste to znovu.<br>An unknown error occurred."
+      x.innerHTML =
+        "Nastala neznámá chyba, zkuste to znovu.<br>An unknown error occurred.";
       break;
   }
 }
-let y = document.getElementById("x");
--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,36 +1,35 @@
 const pridejMistoBtn = document.getElementById("pridej-misto-btn");
 const hledejMistoBtn = document.getElementById("hledej-btn");
-
+var gpsMista = new Object();
 
 
 // Funkce, která vytáhne UL-LI seznam míst z pole misto[]
-  const renderMista =() => {
-    const seznamMist = document.getElementById('seznam-mist');
-  
-    if (misto.length === 0) {
-      seznamMist.classList.remove('visible');
-      return;
-    } else {
-      seznamMist.classList.add('visible');
-    }
-    seznamMist.innerHTML = '';
-  
-    misto.forEach((mista) => {
-      const liSeznamMist = document.createElement('li');
-      let text = mista.info.Místo + ' - ';
-      for (const key in mista.info) {
-        if (key !== 'Místo') {
-          text = text + ` ${key}: ${mista.info[key]}`;
-        }
+const renderMista = () => {
+  const seznamMist = document.getElementById('seznam-mist');
+
+  if (misto.length === 0) {
+    seznamMist.classList.remove('visible');
+    return;
+  } else {
+    seznamMist.classList.add('visible');
+  }
+  seznamMist.innerHTML = '';
+
+  misto.forEach((mista) => {
+    const liSeznamMist = document.createElement('li');
+    let text = mista.info.Místo + ' - ';
+    for (const key in mista.info) {
+      if (key !== 'Místo') {
+        text = text + ` ${key}: ${mista.info[key]}`;
       }
-      liSeznamMist.textContent = text;
-      seznamMist.append(liSeznamMist);
-    });
-  };
-  // Ovladač na button pridej-misto-btn, který vytváří objekt noveMisto a přidává ho do pole misto[]
+    }
+    liSeznamMist.textContent = text;
+    seznamMist.append(liSeznamMist);
+  });
+};
+// Ovladač na button pridej-misto-btn, který vytváří objekt noveMisto a přidává ho do pole misto[]
 const pridejMistoOvladac = () => {
   const popisMista = document.getElementById("vyber-strucny-popis").value;
-
   const druhMista = document.getElementById("vyber-typu-mista").value;
   const nazevMista = document.getElementById("misto").value;
   getLocation();
@@ -58,9 +57,18 @@ const pridejMistoOvladac = () => {
   // nakonec  ovladač i funkci renderMista pro zobrazení v seznamu UL-LI
   renderMista();
 };
+// funkce, která získává poslední souřadnice zadaného místa
+let getLastGps = () => {
+  if (misto[misto.length -1]){
+  gpsMista = misto[misto.length -1];
+  gpsMista = Object.values(gpsMista);
+  gpsMista = Object.values(gpsMista[2]);
+  gpsMista = Object.values(gpsMista);
+  console.log(gpsMista[0], gpsMista[1], gpsMista[2]);
+}else console.log('nic');
+}
+let y = document.getElementById("x");
 
-  
 pridejMistoBtn.addEventListener("click", pridejMistoOvladac);
+hledejMistoBtn.addEventListener('click', getLastGps);
 console.log(misto);
-// console.log(noveMisto);
-// console.log(kratkyPopisMista);
